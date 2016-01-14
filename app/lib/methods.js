39c3bdb0492f2868a73084f52f066986be5be4ef
Daniel Steiert | LMU München <daniel.steiert@gmx.net>
2016-01-14 12:13:05 +0100

    Fix missing dates in XML-output

--- app/lib/methods.js
+++ app/lib/methods.js
@@ -6,9 +6,14 @@ Meteor.methods({
 
   'proc_desc_xml': function(content) {
     if (Meteor.userId() && Meteor.isServer) {
-      var webshot = Meteor.npmRequire('webshot');
       var XML = Meteor.npmRequire('simple-xml');
+      moment.locale('de');
+
       var xmlHeader = '<?xml version="1.0" encoding="UTF-8"?>'
+      content.sectionA.creationDate = moment(content.sectionA.creationDate).format('DD.MM.YYYY');
+      if (content.sectionA.documentPurposeOriginalDate) {
+        content.sectionA.documentPurposeOriginalDate = moment(content.sectionA.documentPurposeOriginalDate).format('DD.MM.YYYY');
+      }
       var xmlDocument = xmlHeader + XML.stringify(content);
 
       return xmlDocument;
