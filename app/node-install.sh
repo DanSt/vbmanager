de257281718ee8b5d9623835ce70672f109474f2
Daniel Steiert | LMU München <daniel.steiert@gmx.net>
2016-05-11 00:00:50 +0200

    Add config files

---
+++ app/node-install.sh
@@ -0,0 +1,10 @@
+# Taken from https://gist.github.com/isaacs/579814
+echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+. ~/.bashrc
+mkdir ~/local
+mkdir ~/node-latest-install
+cd ~/node-latest-install
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+./configure --prefix=~/local
+make install 
+curl https://www.npmjs.com/install.sh | sh
